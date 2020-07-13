7ce35b45a6a2d148b8e558f10cd3ec4ec1f45a02
Sami Loone <sami@loone.fi>
2020-07-13 16:04:15 +0300

    setup: version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="pyostracker",
-    version="0.0.2",
+    version="0.0.3",
     author="ostracker.xyz",
     author_email="admin@ostracker.xyz",
     description="Python client library for ostracker.xyz",
